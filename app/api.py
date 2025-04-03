424b8f25e522785b5d66e7d0e2980b2ec2267a11
marlonjb124 <marlonmarrerorodriguez@gmail.com>
2025-04-02 23:28:21 -0400

    WHook

--- app/api.py
+++ app/api.py
@@ -43,3 +43,7 @@ async def Home():
 async def check_health():
     return{"status":"ok"}
 
+@app.get("webhook-test")
+async def wh(request:Request):
+    print(request.body)
+    print(request.headers)
